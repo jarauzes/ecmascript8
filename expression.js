f2cfdf5e02e2679c761e45a5da17cd7f722c8193
Joel Suarez <admin@webtonator.com>
2019-03-19 20:59:07 +0100

    S3L10: Ecma Expressions

---
+++ expression.js
@@ -0,0 +1,6 @@
+const anExpression = `43 * 1902 = ${43*1902}`;
+console.log(anExpression);
+
+let isMember = true;
+const aTernary = `Your price is ${isMember ? "$2.00" : "$4.00"}`;
+console.log(aTernary); // Your price is $2.00 if isMember = false ; *Your price is $4.00*
